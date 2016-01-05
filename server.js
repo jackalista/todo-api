72f975ab8325f9778becc6499123e131b6ef3885
Jack Hart <jhart@ep.com>
2016-01-04 17:42:48 -0800

    added todos route

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+	id: 1,
+	description: 'go to costco',
+	completed: false
+}, {
+	id: 2,
+	description: 'go to whole foods',
+	completed: false
+}];
 
 app.get('/', function (req, res) {
 	res.send('Todo API Root');
 });
+
+// GET /todos
+app.get('/todos', function (req, res) {
+	res.json(todos);
+});
+
+// GET /todos/:id
 
 app.listen(PORT, function () {
 	console.log('Express listening on port ' + PORT + '!');
