8514c215faa9ddaedf40ed49b1bf99be49ac4fb3
Jack Hart <jhart@ep.com>
2016-01-09 13:35:07 -0800

    added some data validation

--- server.js
+++ server.js
@@ -32,10 +32,14 @@ app.get('/todos/:id', function (req, res) {
 
 // POST /todos
 app.post('/todos', function (req, res) {
-	var body = req.body;
+	var body = _.pick(req.body, 'description', 'completed');
 
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send();
+	} 
 	// add id field
 	body.id = todoNextId++;
+	body.description = body.description.trim();
 	todos.push(body);
 	res.json(body);
 });
