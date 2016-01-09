a226ad0c4bd8e99222d1d78ceb579c8a227d1f4e
Jack Hart <jhart@ep.com>
2016-01-09 11:29:00 -0800

    add todos/:id

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [{
 	id: 1,
-	description: 'go to costco',
+	description: 'go to noahs bagels',
 	completed: false
 }, {
 	id: 2,
