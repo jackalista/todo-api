cf9d40fb20a5928a20b20373ea1ec9f2dfd7a2ec
Jack Hart <jhart@ep.com>
2016-01-09 16:03:05 -0800

    made 404 status return an error object

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.delete('/todos/:id', function (req, res) {
 		todos = _.without(todos, foundTodo);
 		res.json(foundTodo);
 	} else {
-		res.status(404).send();
+		res.status(404).json({"error": "no todo found with that id"});
 	}
 });
 
