55ad987d402c93fc49a36e5ec4e78d47c597e636
Jack Hart <jhart@ep.com>
2016-01-12 15:18:27 -0800

    added query string searching using _.filter

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/', function (req, res) {
 	res.send('Todo API Root');
 });
 
-// GET /todos
+// GET /todos?completed=true&q=kick
 app.get('/todos', function (req, res) {
 	var queryParams = req.query;
 	var filteredTodos = todos;
@@ -28,6 +28,12 @@ app.get('/todos', function (req, res) {
 		filteredTodos = _.where(filteredTodos, validAttributes);
 	} else if (typeof queryParams.completed !== 'undefined' ) {
 		return res.status(400).json({"error": "invalid data submitted"});
+	}
+
+	if(queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+		filteredTodos = _.filter(filteredTodos, function (todo) {
+			return todo.description.indexOf(queryParams.q) > -1;
+		});
 	}
 	
 	res.json(filteredTodos);
